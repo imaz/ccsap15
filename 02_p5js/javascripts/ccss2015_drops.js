019d452f1039cf5ced50428eb044e73159c4eb9f
IMAZU Asami <neko.daisuki.070109@gmail.com>
2015-08-08 18:05:19 +0900

    Copy ccss2015_base to ccss2015_drops

---
+++ 02_p5js/javascripts/ccss2015_drops.js
@@ -0,0 +1,20 @@
+function setup(){
+  createCanvas(800, 200);
+  background(255);
+  fill(233, 233, 233, 255);
+  stroke(100, 100, 100, 255);
+}
+
+function draw(){
+  paths = getPaths();
+  if (paths) {
+    drawSVG(paths);
+  }
+}
+
+function getPaths(){
+  size = 1.0;
+  x = 0;
+  y = 0;
+  return toAbsoluteSVG(CCSSJSON, size, x, y);
+}
