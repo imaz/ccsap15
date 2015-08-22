b1e02c7f3cb865c77c2991260d48a51e693b4413
IMAZU Asami <neko.daisuki.070109@gmail.com>
2015-08-22 17:57:49 +0900

    Drops: マウスのあたりに10コの水玉がパラパラするようにした！

--- 02_p5js/javascripts/ccss2015_drops.js
+++ 02_p5js/javascripts/ccss2015_drops.js
@@ -1,6 +1,7 @@
 var CANVAS_WIDTH = 800;
 var CANVAS_HEIGHT = 200;
 
+var drops = [];
 var paths = getPaths();
 
 function setup(){
@@ -11,6 +12,8 @@ function draw(){
   background(255);
 
   drawLogo(paths);
+
+  updateDrops()
   drawDrops();
 }
 
@@ -29,16 +32,28 @@ function drawLogo(paths){
   }
 }
 
-function drawDrops(){
-  var alpha = random() * 100 + 20;
-  var r = random() * 224 + 30;
-  var g = random() * 224 + 30;
-  var b = random() * 224 + 30;
-  fill(r, g, b, 30);
-  stroke(r, g, b, 50);
+function updateDrops(){
+  var drop = {
+    a: random() * 100 + 20,
+    r: random() * 224 + 30,
+    g: random() * 224 + 30,
+    b: random() * 224 + 30,
+    x: mouseX + (random() * 40 - 20),
+    y: mouseY + (random() * 40 - 20),
+    dot_size: random() * 30 + 5
+  };
+  drops.push(drop);
 
-  var x = mouseX + (random() * 40 - 20);
-  var y = mouseY + (random() * 40 - 20);
-  var dot_size = random() * 30 + 5;
-  ellipse(x, y, dot_size, dot_size)
+  if (drops.length > 10) {
+    drops.shift();
+  }
+}
+
+function drawDrops(){
+  for (drop of drops) {
+    ddd = drop;
+    fill(drop['r'], drop['g'], drop['b'], drop['a']);
+    stroke(drop['r'], drop['g'], drop['b'], drop['a'] + 20);
+    ellipse(drop['x'], drop['y'], drop['dot_size'], drop['dot_size']);
+  }
 }
